1c1e1cbac23408346b6a51d2a8f9ca40810dfa7a
mikeyscode <contact@mikey.codes>
2018-03-09 14:02:37 +0000

    Removing old SendSMS method

--- sms.go
+++ sms.go
@@ -1,13 +1,5 @@
 package nexmo
 
-import (
-	"bytes"
-	"encoding/json"
-	"fmt"
-	"net/http"
-	"time"
-)
-
 // SendMessageEndpoint ...
 const SendMessageEndpoint = "https://rest.nexmo.com /sms/json"
 
@@ -60,47 +52,3 @@ type SMSOptions struct {
 	Callback             string           `json:"callback,omitempty"`
 	MessageClass         DataCodingScheme `json:"message-class,omitempty"`
 }
-
-// SendSMS ... @todo query parameters
-func SendSMS(payload OutboundSMSPayload) (messageDetail *MessageDetail, err error) {
-	jsonPayload, err := json.Marshal(payload)
-	if err != nil {
-		return nil, fmt.Errorf("unable to encode payload as json: %v", err)
-	}
-
-	client := &http.Client{Timeout: time.Second * 15}
-	request, err := http.NewRequest("POST", SendMessageEndpoint, bytes.NewBuffer(jsonPayload))
-	if err != nil {
-		return nil, fmt.Errorf("unable to create post request: %v", err)
-	}
-	request.Header.Set("Content-Type", "application/json")
-
-	response, err := client.Do(request)
-	if err != nil {
-		return nil, fmt.Errorf("unable to process post request: %v", err)
-	}
-	defer response.Body.Close()
-
-	err = json.NewDecoder(response.Body).Decode(&messageDetail)
-	if err != nil {
-		return nil, fmt.Errorf("unable to decode response body: %v", err)
-	}
-
-	return messageDetail, nil
-}
-
-/**
-
-Methods to Handle
-- Send an SMS
-- SendSMS()
-- Path: https://rest.nexmo.com /sms/:format
-- Method: POST
-- Documentation: https://developer.nexmo.com/api/sms#send-an-sms
-- Query Parameters
-	- api_key (required)
-	- api_secret (required if no sig)
-	- sig (required if no api_secret)
-- Request Body
-	-
-*/
