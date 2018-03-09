8b2149794a38e030ceaf7eefcb195648cb0240d4
mikeyscode <contact@mikey.codes>
2018-03-09 20:28:32 +0000

    Putting nexmo test into its own package, to make it clearer of the API an end user would use.

--- nexmo_test.go
+++ nexmo_test.go
@@ -1,22 +1,24 @@
-package nexmo
+package nexmo_test
 
 import (
 	"testing"
 
+	"github.com/mikeyscode/nexmo"
 	"github.com/stretchr/testify/assert"
 )
 
+//
 func TestSetupWithKeyAndSecret(t *testing.T) {
-	n := Nexmo{}
-	n.Setup(Auth{Key: "foo", Secret: "bar"})
+	n := nexmo.Nexmo{}
+	n.Setup(nexmo.Auth{Key: "foo", Secret: "bar"})
 
 	assert.Equal(t, "foo", n.Auth.Key)
 	assert.Equal(t, "bar", n.Auth.Secret)
 }
 
 func TestSetupWithKeyAndSignature(t *testing.T) {
-	n := Nexmo{}
-	n.Setup(Auth{Key: "foo", Signature: "bar"})
+	n := nexmo.Nexmo{}
+	n.Setup(nexmo.Auth{Key: "foo", Signature: "bar"})
 
 	assert.Equal(t, "foo", n.Auth.Key)
 	assert.Equal(t, "bar", n.Auth.Signature)
